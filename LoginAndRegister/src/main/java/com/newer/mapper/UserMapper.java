7382ddead18c8946b42ea2a7d5752a3e0247402e
16665371165 <1666537116@qq.com>
2019-12-25 13:34:45 +0800

    完善usermapper

--- LoginAndRegister/src/main/java/com/newer/mapper/UserMapper.java
+++ LoginAndRegister/src/main/java/com/newer/mapper/UserMapper.java
@@ -1,12 +1,19 @@
 package com.newer.mapper;
 
 import com.newer.domain.User;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @author 梁虔硕版权所有 哈哈哈哈
  * @date 2019/12/25 - 13:01
  */
 public interface UserMapper {
-    //根据用户名查询
+    //根据用户名查询(注册功能)
+    @Select("select * from  user where user_name=#{userName}")
     User findByUserName(String userName);
+
+    //根据用户名和密码查询(登录功能)
+    @Select("select * from  user where user_name=#{userName} adn user_pwd=#{pwd}")
+    User findByUserNameAndpwd(@Param("userName") String userName, @Param("pwd") String pwd );
 }
