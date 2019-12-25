b5533a1f33d6a1b05675191944f277aa2fd032fb
16665371165 <1666537116@qq.com>
2019-12-25 13:14:02 +0800

    添加servlet 处理登录注册请求

---
+++ LoginAndRegister/src/main/java/com/newer/servlet/LogRegServlet.java
@@ -0,0 +1,13 @@
+package com.newer.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author 梁虔硕版权所有 哈哈哈哈
+ * @date 2019/12/25 - 13:12
+ * 处理登录注册请求
+ */
+@WebServlet("/LogRegServlet")
+public class LogRegServlet extends HttpServlet {
+}
