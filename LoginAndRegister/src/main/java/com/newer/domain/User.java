801537a529531b6fc441240ba41bf83eb98df513
16665371165 <1666537116@qq.com>
2019-12-25 13:22:10 +0800

    给user实体类加字段

--- LoginAndRegister/src/main/java/com/newer/domain/User.java
+++ LoginAndRegister/src/main/java/com/newer/domain/User.java
@@ -3,6 +3,53 @@ package com.newer.domain;
 /**
  * @author 梁虔硕版权所有 哈哈哈哈
  * @date 2019/12/25 - 13:02
+ *
  */
 public class User {
+    //占时只有这些字段 后期根据功能在加
+    private  Integer uid; //用户id
+    private  String userName;//用户名
+    private  String  password;//用户密码
+    private  String email;//用户邮箱
+    private  String   phone;//手机号码
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
